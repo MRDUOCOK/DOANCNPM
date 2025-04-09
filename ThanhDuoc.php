54af5b50e9991c8ab20d72b07f2b88475e0187e9
MRDUOCOK <ngoduoc86@gmail.com>
2025-04-09 14:49:44 +0700

    Delete ThanhDuoc.php

--- ThanhDuoc.php
+++
@@ -1,5 +0,0 @@
-//US1:Là một người dùng, tôi muốn hệ thống hiển thị đánh giá và bình luận của người khác để tham khảo trước khi mua
-//US2:Là một người dùng, tôi muốn được chọn hình thức thanh toán như chuyển khoản, COD hoặc ví điện tử
-//US3:Là một người dùng, tôi muốn theo dõi trạng thái giao hàng để biết đơn hàng của mình đang ở đâu
-//US4:Là một người dùng, tôi muốn được đánh giá sản phẩm sau khi nhận hàng
-//US5:Là một người dùng, tôi muốn nhận thông báo khuyến mãi qua email hoặc ứng dụng
